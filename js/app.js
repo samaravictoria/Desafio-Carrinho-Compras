500d3710904a97b3073178b4ebf9f60b8d113645
Samara Victoria <Maia_victoria@outlook.com.br>
2024-12-04 14:53:21 -0300

    Recupera valores de Nome do Produto e Quantidade

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 function adicionar () {
 //recuperar valores: nome do produto, quantidade e valor;  
+    let nomeProduto = document.getElementById('produto');   
+    let quantidadeProduto = document.getElementById('quantidade'); 
+
+    // Alert teste:
+    // alert(nomeProduto.value);
+    // alert(quantidadeProduto.value);
+    
 //calcular o preço = sub total; 
+
 //adicionar o produto no carrinho;
 //atualizar o total da compra; 
 }
