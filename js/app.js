a48adee73fead039d3cb6c5a325a546164c43f3b
Samara Victoria <Maia_victoria@outlook.com.br>
2024-12-04 19:28:21 -0300

    Adiciona items no carrinho

--- js/app.js
+++ js/app.js
@@ -3,19 +3,22 @@ function adicionar () {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1]; 
-    let quantidade = document.getElementById('quantidade');
+    let quantidade = document.getElementById('quantidade').value;
 
         // Alert teste:
-        alert(nomeProduto);
-        alert(valorUnitario);
-        alert(quantidade.value);
+            // alert(nomeProduto);
+            // alert(valorUnitario);
+            // alert(quantidade.value);
   
         
 //calcular o preço = sub total; 
     let preco = quantidade.value * valorUnitario; 
+    let carrinho = document.getElementById('lista-produtos');  
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span> </section>`;
 
         // Alert teste:
-            alert(preco); 
+            // alert(preco); 
 
 
 //adicionar o produto no carrinho;
