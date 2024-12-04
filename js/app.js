e97ef9a753973257e202ea3a99a33161e9eac8e3
Samara Victoria <Maia_victoria@outlook.com.br>
2024-12-04 19:17:12 -0300

    Adição Recuperação de Valores: Preço + Cálculo Sub total.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 function adicionar () {
 //recuperar valores: nome do produto, quantidade e valor;  
-    let nomeProduto = document.getElementById('produto');   
-    let quantidadeProduto = document.getElementById('quantidade'); 
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1]; 
+    let quantidade = document.getElementById('quantidade');
 
-    // Alert teste:
-    // alert(nomeProduto.value);
-    // alert(quantidadeProduto.value);
-    
+        // Alert teste:
+        alert(nomeProduto);
+        alert(valorUnitario);
+        alert(quantidade.value);
+  
+        
 //calcular o preço = sub total; 
+    let preco = quantidade.value * valorUnitario; 
+
+        // Alert teste:
+            alert(preco); 
+
 
 //adicionar o produto no carrinho;
 //atualizar o total da compra; 
