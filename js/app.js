d7b7b47fa924de40b49e2634be08b6f376309ab4
Samara Victoria <Maia_victoria@outlook.com.br>
2024-12-04 14:46:14 -0300

    Inicio

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function adicionar () {
+//recuperar valores: nome do produto, quantidade e valor;  
+//calcular o preço = sub total; 
+//adicionar o produto no carrinho;
+//atualizar o total da compra; 
+}
+
+function limpar () { 
+}
